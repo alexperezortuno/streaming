b34e904ed244f242adb93cc2be699aea648a22c0
alexperezortuno <alexperezortuno@gmail.com>
2026-07-21 21:45:55 -0400

    Delete unused files and components related to the video app.

--- app/api/version.go
+++
@@ -1,5 +0,0 @@
-package api
-
-var version = "0.1.0"
-var port = "3000"
-var host = ""
--- app/datatable.sql
+++
@@ -1,8 +0,0 @@
-create table list
-(
-    id INTEGER
-        constraint list_pk
-            primary key autoincrement,
-    name varchar(100) default NULL not null
-);
-
